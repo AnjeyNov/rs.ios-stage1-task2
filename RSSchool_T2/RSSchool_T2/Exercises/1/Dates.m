5cfe700b5cf11787e5f88c14f07488f168b0e8d0
Anjey Novicki <anjeynov@gmail.com>
2020-03-21 17:48:57 +0300

    feat: Part 1 done

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,24 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.locale = [NSLocale localeWithLocaleIdentifier:@"ru_RU"];
+    formatter.dateFormat = @"yyyy-MM-dd";
+    NSMutableString *stringDate = [NSMutableString new];
+    [stringDate appendString:year];
+    [stringDate appendString:@"-"];
+    [stringDate appendString:month];
+    [stringDate appendString:@"-"];
+    [stringDate appendString:day];
+    [formatter setTimeZone:[[NSTimeZone alloc] initWithName:@"GMT+00:00"]];
+    NSDate *date = [formatter dateFromString:stringDate];
+    formatter.dateFormat = @"d MMMM, EEEE";
+    NSString *result = [formatter stringFromDate:date];
+    if(result){
+        return result;
+    } else {
+        return @"Такого дня не существует";
+    }
 }
 
 @end
