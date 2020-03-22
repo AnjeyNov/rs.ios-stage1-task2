651674b9813ff02dc2873d71b617e184998b9d9c
Anjey Novicki <anjeynov@gmail.com>
2020-03-22 12:53:05 +0300

    feat: Part 3 done

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,30 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    if([a length] == 0 || [b length] == 0) {
+        return @"NO";
+    }
+    
+    NSString *_a = [a lowercaseString];
+    NSString *_b = [b lowercaseString];
+    if([_a containsString:_b]){
+        return @"YES";
+    }
+    
+    NSString *symbolInFirstString;
+    NSInteger index = 0;
+    NSMutableString *trimmerString = [NSMutableString new];
+    for(; index < [_a length]; index++){
+        symbolInFirstString = [_a substringWithRange:NSMakeRange(index, 1)];
+        if([_b containsString:symbolInFirstString]){
+            [trimmerString appendString:symbolInFirstString];
+        }
+    }
+    
+    if([trimmerString containsString:_b]){
+        return @"YES";
+    }
+    
+    return @"NO";
 }
 @end
