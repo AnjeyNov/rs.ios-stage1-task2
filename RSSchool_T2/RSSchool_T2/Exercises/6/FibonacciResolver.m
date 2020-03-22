74fdaa593198aea400901a177f723a98351ac5eb
Anjey Novicki <anjeynov@gmail.com>
2020-03-22 13:35:34 +0300

    feat: Part 6 done

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,30 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
+    NSMutableArray<NSNumber*>* fibonacci = [[NSMutableArray alloc] initWithArray:@[@0, @1] copyItems:YES];
+    NSMutableArray<NSNumber*>* result = [NSMutableArray new];
+    BOOL flag = YES;
+    NSInteger index = 1;
+    do {
+        NSNumber *value = [[NSNumber alloc] initWithInt:([fibonacci[index] intValue] * [fibonacci[index-1] intValue])];
+        if([number intValue] == [value intValue]){
+            [result addObject:fibonacci[index-1]];
+            [result addObject:fibonacci[index]];
+            [result addObject: @1];
+            return result;
+        }
+        if([number intValue] > [value intValue]){
+            [fibonacci addObject:[[NSNumber alloc] initWithInt:([fibonacci[index] intValue] + [fibonacci[index-1] intValue])]];
+            index++;
+            continue;
+        }
+        if([number intValue] < [value intValue]){
+            [result addObject:fibonacci[index-1]];
+            [result addObject:fibonacci[index]];
+            [result addObject: @0];
+            return result;
+        }
+    } while (flag);
     return @[];
 }
 @end
